2cd83b607a28e4d86b2c88fcf27005f2d4b0a105
Michael Catanzaro <catanzaromj@pm.me>
2022-05-02 20:09:14 -0400

    Move nonTDA svm

--- main.py
+++ main.py
@@ -21,17 +21,17 @@ the workflow begins with reading from perseus input files which have been provid
 in 'data/postprocessed'.
 """
 
-from src import target_labels
-from src.make_dataset import construct_vector
+import pandas as pd
+import seaborn as sns
+from src import target_labels as TARGET_LABELS
 from src.landscapes import (
     construct_landscapes,
 )
-from src.permutation_test import permutation_test
-from src.svm import landscape_svm, raw_svm
+from src.svm import landscape_svm
 
 SUPRA_LEVEL = True  # Compute the supralevel persistent homology as opposed to sub-level
-DATA_DIR = "data/"  # edit this to point to the path of the data directory.
-SUBJECT_LIST = ["0508"]
+DATA_DIR = "/run/user/1000/gvfs/smb-share:server=192.168.68.110,share=home/Sam_Vaibhav_project_files/"  # edit this to point to the path of the data directory"  # edit this to point to the path of the data directory.
+SUBJECT_LIST = ["0295", "0394", "0408", "0484", "0489", "0505", "0521", "0551"]
 HOMOLOGICAL_DEGREES = [0, 1]
 
 PERM_TEST_LABELS = ["rest", "beat"]
@@ -46,56 +46,83 @@ SVM_TEST_LABELS = ["rest", "beat"]
 # construct_perseus_input_files(SUBJECT_LIST, supra=SUPRA_LEVEL)
 # run perseus on those files
 
-pl_list = []
-for subject in SUBJECT_LIST:
-    for hom_deg in HOMOLOGICAL_DEGREES:
-        pl_list.append(
-            construct_landscapes(subject=subject, hom_deg=hom_deg, data_dir=DATA_DIR)
-        )
-
 # Permutation Test; example call
-p_val0 = permutation_test(pl_list[0], PERM_TEST_LABELS)
-p_val1 = permutation_test(pl_list[1], PERM_TEST_LABELS)
+# p_val0 = permutation_test(pl_list[0], PERM_TEST_LABELS)
+# p_val1 = permutation_test(pl_list[1], PERM_TEST_LABELS)
+
+
+avg_accur = pd.DataFrame(
+    columns=["Rest vs Beat", "Random vs Beat", "Rest vs Random", "All Three"]
+)
+
+for subject in SUBJECT_LIST:
+    pl_list = []
+    for hom_deg in HOMOLOGICAL_DEGREES:
+        pl = construct_landscapes(subject=subject, hom_deg=hom_deg, data_dir=DATA_DIR)
+        pl_list.append(pl)
+    svm_vals = []
+    svm_vals.append(
+        landscape_svm(
+            landscapes=pl_list[0] + pl_list[1],
+            labels=["rest", "beat"],
+            target_labels=TARGET_LABELS * 2,
+        ).mean()
+    )
+    svm_vals.append(
+        landscape_svm(
+            landscapes=pl_list[0] + pl_list[1],
+            labels=["random", "beat"],
+            target_labels=TARGET_LABELS * 2,
+        ).mean()
+    )
+    svm_vals.append(
+        landscape_svm(
+            landscapes=pl_list[0] + pl_list[1],
+            labels=["rest", "random"],
+            target_labels=TARGET_LABELS * 2,
+        ).mean()
+    )
+    svm_vals.append(
+        landscape_svm(
+            landscapes=pl_list[0] + pl_list[1],
+            labels=["rest", "beat", "random"],
+            target_labels=TARGET_LABELS * 2,
+        ).mean()
+    )
+    avg_accur.loc[subject] = svm_vals
+ax = sns.heatmap(avg_accur, annot=True, linewidths=0.5, cmap="YlOrRd_r")
+ax.set_title("TDA-based SVM classification accuracies as a function of pairing")
+ax.set_xticklabels(avg_accur.columns)
 
 
 # SVM
 # Any of the classifiers below can be uncommented.
 
 # Build a classifier using H0
-svm_random_rest_0, score_random_rest_0 = landscape_svm(
-    landscapes=pl_list[0],
-    labels=SVM_TEST_LABELS,
-    target_labels=target_labels,
-    folds=10,
-)
-
-svm_raw_random_rest_0, score_raw_random_rest_0 = raw_svm(
-    list_of_vectors=construct_vector(subject=SUBJECT_LIST[0],
-                                     hom_deg=0,
-                                     data_dir=DATA_DIR),
-    labels=SVM_TEST_LABELS,
-    target_labels=target_labels,
-    folds=10
-)
-
-# Build a classifier using H1
-svm_random_rest_1, score_random_rest_1 = landscape_svm(
-    landscapes=pl_list[1],
-    labels=SVM_TEST_LABELS,
-    target_labels=target_labels,
-    folds=10,
-)
-
-print(score_raw_random_rest_0)
-# # Build a classifier using H2
-# svm_random_rest_2, score_random_rest_2 = landscape_svm(
-#     landscapes=pl_list[2],
+# svm_random_rest_0, score_random_rest_0 = landscape_svm(
+#     landscapes=pl_list[0],
 #     labels=SVM_TEST_LABELS,
 #     target_labels=target_labels,
 #     folds=10,
 # )
 
-# Build a classifier using both H0 and H1
+# # Build a classifier using H1
+# svm_random_rest_1, score_random_rest_1 = landscape_svm(
+#     landscapes=pl_list[1],
+#     labels=SVM_TEST_LABELS,
+#     target_labels=target_labels,
+#     folds=10,
+# )
+
+# # # Build a classifier using H2
+# # svm_random_rest_2, score_random_rest_2 = landscape_svm(
+# #     landscapes=pl_list[2],
+# #     labels=SVM_TEST_LABELS,
+# #     target_labels=target_labels,
+# #     folds=10,
+# # )
+
+# # Build a classifier using both H0 and H1
 # svm_random_rest_01, score_random_rest_01 = landscape_svm(
 #     landscapes=pl_list[0] + pl_list[1],
 #     labels=SVM_TEST_LABELS,
@@ -121,6 +148,16 @@ print(score_raw_random_rest_0)
 #         num_steps=pl_list[1][i].num_steps,
 #         hom_deg=pl_list[1][i].hom_deg,
 #         values=pl_list[1][i][:5],
+#     )
+#     for i in range(len(pl_list[1]))
+# ]
+# svm_random_rest_trunc_01, score_random_rest_trunc_01 = landscape_svm(
+#     landscapes=truncated_pl0 + truncated_pl1,
+#     labels=SVM_TEST_LABELS,
+#     target_labels=target_labels * 2,
+#     folds=10,
+#     C=100,
+# )
 #     )
 #     for i in range(len(pl_list[1]))
 # ]
